16c1c372ba922dddb63d970c056c5b6575e61be9
GK <Gikoskos@users.noreply.github.com>
2015-07-29 15:20:52 +0300

    fixed spaces

--- tinymailclient.pl
+++ tinymailclient.pl
@@ -2,11 +2,11 @@ use strict;
 use warnings;
 use Email::Send::SMTP::Gmail;
 my $mail=Email::Send::SMTP::Gmail->new( -smtp=>'smtp.gmail.com',
-								-login=>'example@gmail.com',
-								-pass=>'this_is_your_password');
+					-login=>'example@gmail.com',
+					-pass=>'this_is_your_password');
 
 $mail->send(-to=>'example@example.example', 
-		-subject=>'SUBJECT',
-		-body=>'ALARM',
-		-contenttype=>'text/html');
+	-subject=>'SUBJECT',
+	-body=>'ALARM',
+	-contenttype=>'text/html');
 $mail->bye;
